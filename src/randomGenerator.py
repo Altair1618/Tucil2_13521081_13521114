65ef6060880098abbfc8e564905759b862df354f
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2023-02-23 00:38:36 +0700

    add-RandomGenerator-module

---
+++ src/randomGenerator.py
@@ -0,0 +1,21 @@
+import random
+from point import Point
+
+# Konstanta Maksimal Nilai Koordinat
+MAXN = 100
+
+
+def generateRandomPoints(num, dimension):
+    # Menghasilkan Titik Random Sebanyak num Berdimensi dimension
+    # Dipastikan Semua Titik Unik
+    listPoint = []
+    for i in range(num):
+        while True:
+            coordinates = []
+            for j in range(dimension):
+                coordinates += [random.randint(0, MAXN)]
+            if Point not in listPoint:
+                break
+        listPoint += [Point(coordinates)]
+
+    return listPoint
